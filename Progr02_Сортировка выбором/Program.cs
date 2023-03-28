7cca67d8fb6d558abffbc6fc68906664bf48bd21
AnnaVlady <7753277av@gmail.com>
2023-03-28 18:51:22 +0300

    added sort

---
+++ Progr02_Сортировка выбором/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+
+//int n = Random.Shared.Next(1000000); // логарифмическая сложность
+
+double s = 0;
+
+for (int n = 0; n < 15; n++)
+{
+    int count = 0;
+    int i = n;
+    while (i > 0)
+    {
+        count++;
+        s += i;
+        i = i / 2;
+    }
+    Console.WriteLine($"n: {n}  count: {count}");
+}
+
+// 
+// Console.WriteLine("++");
